532dd9339c063772f19d79f04bc57211927122fb
Gabriel <caampos22@gmail.com>
2023-09-01 20:41:50 -0300

    feat ✨: Add local database sync

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from '@clerk/nextjs'
 export default authMiddleware({
   // "/" will be accessible to all users
   publicRoutes: ['/'],
+  ignoredRoutes: ['/api/webhooks/users'],
 })
 
 export const config = {
